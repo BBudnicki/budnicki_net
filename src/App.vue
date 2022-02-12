2f2febaa2da4a63f8d0fd20622bb68d92f081f07
BBudnicki <brandon@Budnicki.net>
2022-02-12 16:25:01 -0700

    initial packages installed

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Content></Content>
+    <Map></Map>
+
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import Content from "./components/content/Content.vue";
+import Map from "./components/map/Map.vue";
+
 
 @Component({
   components: {
-    HelloWorld,
+    Content,
+    Map
   },
 })
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,23 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import map from "./map";
 
 Vue.use(Vuex);
-
+const seconds = 1000;
+const minutes = 60 * seconds;
+const hours = 60 * minutes;
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+
+  },
+  mutations: {
+
+  },
+  actions: {
+
+
+  },
+  modules: {
+    map: map,
+  },
 });
